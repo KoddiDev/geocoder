db7875c1ca0fb015625ce322a6267726620fcd92
Matthew Cross <matthew@koddi.com>
2017-09-08 10:44:07 -0500

    Update tests to handle zero result logic

--- src/test/scala/com/koddi/geocoder/GeocoderSpec.scala
+++ src/test/scala/com/koddi/geocoder/GeocoderSpec.scala
@@ -17,26 +17,12 @@ class GeocoderSpec extends TestSpec {
         loseAccuracy(location.longitude) should be(-97)
     }
 
-    it should "throw an exception when an invalid address is given" in {
-        val geocoder = new MockGeocoder("api_response_invalid.xml")
-        an [InvalidLocationException] should be thrownBy {
-            geocoder.lookup("Nowhere NONE INVALID ADDRESS")
-        }
-    }
-
     it should "reverse lookup lat/lng values and return a sequence of Results" in {
         val geocoder = new MockGeocoder("api_response_latlng.xml")
         val results = geocoder.lookup(32.7505842, -97.3574015)
         val address = results.head.formattedAddress
 
         address should be("2821 W 7th St, Fort Worth, TX 76107, USA")
-    }
-
-    it should "throw an exception when an invalid lat/lng is given" in {
-        val geocoder = new MockGeocoder("api_response_invalid.xml")
-        an [InvalidLocationException] should be thrownBy {
-            geocoder.lookup(-900d, -900d)
-        }
     }
 
     it should "lookup an address by component objects" in {
@@ -50,6 +36,19 @@ class GeocoderSpec extends TestSpec {
 
         loseAccuracy(location.latitude) should be(33)
         loseAccuracy(location.longitude) should be(-97)
+    }
+
+    it should "return an empty result sequence when the response has zero results" in {
+        val geocoder = new MockGeocoder("api_response_zero_results.xml")
+        val results = geocoder.lookup("Nowhere NONE INVALID ADDRESS")
+        results should be(Seq.empty[Result])
+    }
+
+    it should "throw an exception when an invalid lat/lng is given" in {
+        val geocoder = new MockGeocoder("api_response_invalid.xml")
+        an [InvalidLocationException] should be thrownBy {
+            geocoder.lookup(-900d, -900d)
+        }
     }
 
     private def loseAccuracy(value: Double): Long = Math.round(value)
